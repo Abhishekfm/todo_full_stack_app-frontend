4a403bb4a90014f828d7f1e6d3f688cc1de1cfef
Abhishek Singh <62643405+Abhishekfm@users.noreply.github.com>
2022-12-23 06:57:05 +0000

    SignUp

--- src/component/SignUp.jsx
+++ src/component/SignUp.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, {useState} from "react";
-import { toast } from "react-hot-toast";
+import toast, { Toaster } from 'react-hot-toast';
 import { useNavigate } from "react-router-dom";
 
 export const SignUp = () => {
@@ -23,9 +23,9 @@ export const SignUp = () => {
             }
 
             const res = await axios.post("http://localhost:4000/api/createUser",{
-                name,
-                email,
-                password
+                name: name,
+                email: email,
+                password: password
             })
 
             if(!res){
@@ -34,8 +34,12 @@ export const SignUp = () => {
             }
 
             if(res.data.success){
+                setName("")
+                setEmail("")
+                setPassword("")
                 navigate("/home")
                 toast.success("Successfully created a account")
+                return
             }
 
         } catch(err){
@@ -48,11 +52,12 @@ export const SignUp = () => {
                 <div className="text-center">
                     <h2 className="font-semibold text-lg" >Sign Up</h2>
                 </div>
-                <input type="text" onChange={(e)=>setName(e.target.value)} className="pl-[4px] rounded-md border-2 border-slate-400 shadow-black" placeholder="Enter Your Name" name="" id="" />
+                <input type="text" onChange={(e)=>setName(e.target.value)} className="pl-[4px] rounded-md border-2 border-slate-400 shadow-black" placeholder="Enter Your Name" name="" />
                 <input type="email" onChange={(e)=>setEmail(e.target.value)} className="pl-[4px] rounded-md border-2 border-slate-400 shadow-black" placeholder="Enter Your Email" />
-                <input type="password" onChange={(e)=>setPassword(e.target.value)} className="pl-[4px] rounded-md border-2 border-slate-400 shadow-black" placeholder="Enter Your Password" name="" id="" />
+                <input type="password" onChange={(e)=>setPassword(e.target.value)} className="pl-[4px] rounded-md border-2 border-slate-400 shadow-black" placeholder="Enter Your Password" name="" />
                 <button className="px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">SignUp</button>
             </form>
+            <Toaster />
         </div>
     )
 }
